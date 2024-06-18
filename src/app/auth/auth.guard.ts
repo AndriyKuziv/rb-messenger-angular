0bfbc1599c8fa9553de8440a496a6bf4ed401024
Andrii Kuziv <happyandreas18@gmail.com>
2024-06-18 17:10:54 +0300

    Improved authGuard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,8 +1,30 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  if (localStorage.getItem('token')){
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  if (token && isValid(token)){
     return true;
   }
+
+  authService.removeToken();
+  inject(Router).navigate(['/login']);
+
   return false;
 };
+
+function isValid(token: string){
+  const tokenPayload = parseJwt(token);
+  return tokenPayload && new Date(tokenPayload.exp * 1000) > new Date();
+}
+
+function parseJwt(token: string) {
+  try {
+    return JSON.parse(atob(token.split('.')[1]));
+  } catch (e) {
+    return null;
+  }
+}
